3765dfc7be1fa1d8a8fff1e91b458d843de2a03f
Santiago <sagos0919@gmail.com>
2023-02-26 18:07:18 +0000

    About page created

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,83 @@
+import {
+	Box,
+	Card,
+	CardBody,
+	CardFooter,
+	CardHeader,
+	Container,
+	Flex,
+	Heading,
+	VStack,
+	HStack,
+	Image,
+	Text,
+	Icon,
+} from '@chakra-ui/react';
+
+import {
+	html_5_logo,
+	css_logo,
+	js_logo,
+	bootstrap_logo,
+	nodejs_logo,
+	react_logo,
+	selfie,
+} from '../assets/export';
+
 const About = () => {
-	return <></>;
+	return (
+		<Flex direction={'column'} justify={'space-evenly'} align={'center'}>
+			<Container>
+				<Card id="about-card">
+					<CardHeader>
+						<Image id="selfie" alt="That's me" src={selfie} />
+					</CardHeader>
+					<CardBody>
+						<Text className="card-text">
+							<Text as={'span'} id="greeting">
+								Hello again!{' '}
+							</Text>
+							I am part of that generation that started to have access to
+							internet and not only a computer but different devices (of which,
+							several went to the technician.), and there has been a huge gap
+							between use and knowledge about technology.
+						</Text>
+						<Text className="card-text">
+							What captured my attention and started to make many questions,
+							leading me into more and more doubts. And thus, in search for
+							those answers I started to feel some attraction to discover the
+							huge amount of things to do and why not, create.
+						</Text>
+						<Text className="card-text">
+							So, feel free to check on anything you find!
+						</Text>
+					</CardBody>
+					<CardFooter>
+						<Box
+							as="button"
+							href="https://drive.google.com/file/d/1k5YBTciucnt3N5Dxceq2FNJp9iv7Y21l/view?usp=sharing"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Resume
+						</Box>
+					</CardFooter>
+				</Card>
+			</Container>
+			<VStack id="display-skills">
+				<Heading as={'h3'}>Skills</Heading>
+				<Text>I'm well experienced in this technologies...</Text>
+				<HStack id="skills-set">
+					<Image src={html_5_logo} alt="HTML5 logo" />
+					<Image src={css_logo} alt="CSS logo" />
+					<Image src={js_logo} alt="JavaScript logo" />
+					<Image src={bootstrap_logo} alt="Bootstrap logo" />
+					<Image src={nodejs_logo} alt="Node.js logo" />
+					<Image src={react_logo} alt="React logo" />
+				</HStack>
+			</VStack>
+		</Flex>
+	);
 };
 
 export default About;
