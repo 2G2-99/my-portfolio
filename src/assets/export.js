e27f748676afc3d290af744eba66e89440768d98
Santiago <sagos0919@gmail.com>
2023-02-26 11:54:44 +0000

    Data and assets added

---
+++ src/assets/export.js
@@ -0,0 +1,34 @@
+// # IMPORTS OF ALL SVGs
+import HTML5Logo from './svg/html-5-svgrepo-com.svg';
+import CSSLogo from './svg/css-3-svgrepo-com.svg';
+import JSLogo from './svg/javascript-svgrepo-com.svg';
+import GITLogo from './svg/git-svgrepo-com.svg';
+import GitHubLogo from './svg/github-svgrepo-com.svg';
+import JQueryLogo from './svg/jquery-svgrepo-com.svg';
+import BootstrapLogo from './svg/bootstrap-svgrepo-com.svg';
+import NodejsLogo from './svg/node-js-svgrepo-com.svg';
+import GoTo from './svg/internet-www-svgrepo-com.svg';
+import Linkedin from './svg/linkedin-svgrepo-com.svg';
+import mailEnvelope from './svg/email-svgrepo-com.svg';
+import pdfFile from './svg/pdf-svgrepo-com.svg';
+
+// # IMPORTING IMAGES
+import selfie from './images/me.JPEG';
+
+// # EXPORTING ALL ASSETS TOGETHER FOR AN EASIER IMPORTING PROCESS
+
+export {
+	HTML5Logo,
+	CSSLogo,
+	JSLogo,
+	GITLogo,
+	GitHubLogo,
+	JQueryLogo,
+	BootstrapLogo,
+	NodejsLogo,
+	GoTo,
+	Linkedin,
+	mailEnvelope,
+	pdfFile,
+	selfie,
+};
