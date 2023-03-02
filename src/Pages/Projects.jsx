31cf4def4d30c0913c67c452bdfebb1ad15594d0
Santiago <sagos0919@gmail.com>
2023-03-02 22:31:54 +0000

    Project Cards added to Projects page

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,5 +1,47 @@
+import {
+	Box,
+	Container,
+	Heading,
+	Image,
+	SimpleGrid,
+	Tooltip,
+} from '@chakra-ui/react';
+import Display from '../components/Main/Card';
+
+import listOfProjects from '../data/listOfProjects.json';
+
 const Projects = () => {
-	return <></>;
+	return (
+		<Container maxW={'container.xl'} my={'2rem'} py={'1rem'}>
+			<SimpleGrid columns={{ base: '1', sm: '2', md: '3' }} spacing={'1.5rem'}>
+				{listOfProjects.map(project => {
+					return (
+						<Display
+							key={project.id}
+							title={project.title}
+							picture={`${project.picture}`}
+							description={project.description}
+							technologies={project.technologies.map(technology => {
+								return technology ? (
+									<Box key={technology.id} className="technology">
+										<Tooltip label={`${technology.technology}`}>
+											<Image
+												className="tech-icon"
+												src={`${technology.path}`}
+												alt={`Logo of ${technology.technology}`}
+											/>
+										</Tooltip>
+									</Box>
+								) : null;
+							})}
+							repository={project.repository}
+							deployed={project.deployed}
+						/>
+					);
+				})}
+			</SimpleGrid>
+		</Container>
+	);
 };
 
 export default Projects;
