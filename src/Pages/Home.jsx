2cdad9e5c3bbebce5a0ccbe51180521c64d52a74
Santiago <sagos0919@gmail.com>
2023-03-25 18:18:18 +0000

    Height on Main and Navbar fixed now occupying the whole page and Home is also styled

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,26 +1,11 @@
-import { Container, Flex, Link, Text } from '@chakra-ui/react';
+import { Container, Link, Text } from '@chakra-ui/react';
 import { Hero } from '../components/export';
 
 const Home = () => {
 	return (
-		<Flex
-			id="home"
-			wrap={'wrap'}
-			direction={'column'}
-			justify={'center'}
-			position={'relative'}
-			h={'100%'}
-		>
-			<Container
-				backdropFilter="auto"
-				backdropContrast={'50%'}
-				boxShadow={'inner'}
-				py={'2'}
-				borderRadius={'md'}
-			>
-				<Hero />
-			</Container>
-			<Text display={'block'} position={'absolute'} right={'0'} bottom={'0'}>
+		<Container id="home">
+			<Hero />
+			<Text position={'absolute'} right={'1'} bottom={'0'}>
 				Background by: {/* */}
 				<Link
 					id="attribution-link"
@@ -31,7 +16,7 @@ const Home = () => {
 					SVGBackgrounds.com
 				</Link>
 			</Text>
-		</Flex>
+		</Container>
 	);
 };
 
