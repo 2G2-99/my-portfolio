1689c145167610b56ef8d43225fd45e4b3f16a7f
Santiago <sagos0919@gmail.com>
2023-06-28 13:07:59 +0100

    Decided to remove the progress bar from the SkillIcon components

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,15 +1,4 @@
-import {
-	Container,
-	Flex,
-	Heading,
-	VStack,
-	Text,
-	Tooltip,
-	Icon,
-	Progress,
-	Box,
-} from '@chakra-ui/react';
-
+import { Container, Flex, Heading, Text, HStack } from '@chakra-ui/react';
 import {
 	IoLogoCss3,
 	IoLogoJavascript,
@@ -18,11 +7,35 @@ import {
 } from 'react-icons/io';
 import { IoLogoReact } from 'react-icons/io5';
 import { BsBootstrapFill } from 'react-icons/bs';
-import { SiChakraui } from 'react-icons/si';
+import { SiChakraui, SiFigma } from 'react-icons/si';
 import Info from '../components/Main/Card/Info';
-import SkillProgress from '../components/Main/SkillProgress';
+import SkillIcon from '../components/Main/SkillIcon';
+import listOfSkills from '../data/listOfSkills.json';
 
 export default function About() {
+	const listOfIcons = {
+		IoLogoCss3,
+		IoLogoJavascript,
+		IoLogoNodejs,
+		IoLogoHtml5,
+		IoLogoReact,
+		BsBootstrapFill,
+		SiChakraui,
+		SiFigma,
+	};
+
+	const getSkillInfo = (skill) => {
+		const IconComponent = listOfIcons[skill.icon];
+		return (
+			<SkillIcon
+				key={skill.id}
+				tooltipText={skill.tooltipText}
+				icon={IconComponent}
+				iconLabel={skill.iconLabel}
+			/>
+		);
+	};
+
 	return (
 		<Flex
 			py={4}
@@ -35,19 +48,17 @@ export default function About() {
 				<Info
 					image={'/images/me.jpeg'}
 					text={
-						<>
-							<Text className="card-text">
-								As a Junior Front-End Developer, I have a solid understanding of
-								HTML, CSS, and Javascript. But I'm always looking for ways to
-								improve my skills and knowledge. So, I joined a bootcamp program
-								where I was able to deepen my understanding of the fundamentals
-								and work with a team using different libraries and frameworks.
-								My ultimate goal is to create visually appealing and responsive
-								websites with efficient and effective code. I know the industry
-								is constantly changing, and I'm excited to stay up-to-date with
-								the latest technologies.
-							</Text>
-						</>
+						<Text className="card-text">
+							As a Junior Front-End Developer, I have a solid understanding of
+							HTML, CSS, and Javascript. But I'm always looking for ways to
+							improve my skills and knowledge. So, I joined a bootcamp program
+							where I was able to deepen my understanding of the fundamentals
+							and work with a team using different libraries and frameworks. My
+							ultimate goal is to create visually appealing and responsive
+							websites with efficient and effective code. I know the industry is
+							constantly changing, and I'm excited to stay up-to-date with the
+							latest technologies.
+						</Text>
 					}
 					resume={
 						'https://drive.google.com/file/d/1UUgXIvDEwrmKPSQ2T1jJWpljLJeTf8tV/view?usp=sharing'
@@ -72,48 +83,9 @@ export default function About() {
 					<Text textAlign={'center'}>
 						Here is my stack of most used technologies.
 					</Text>
-					<SkillProgress
-						tooltipText={'HTML5'}
-						icon={IoLogoHtml5}
-						iconLabel={'HTML5 logo'}
-						progressOnSkill={90}
-					/>
-					<SkillProgress
-						tooltipText={'CSS3'}
-						icon={IoLogoCss3}
-						iconLabel={'CSS3 logo'}
-						progressOnSkill={80}
-					/>
-					<SkillProgress
-						tooltipText={'JavaScript'}
-						icon={IoLogoJavascript}
-						iconLabel={'JavaScript logo'}
-						progressOnSkill={80}
-					/>
-					<SkillProgress
-						tooltipText={'Bootstrap'}
-						icon={BsBootstrapFill}
-						iconLabel={'Bootstrap logo'}
-						progressOnSkill={60}
-					/>
-					<SkillProgress
-						tooltipText={'Chakra UI'}
-						icon={SiChakraui}
-						iconLabel={'Chakra UI logo'}
-						progressOnSkill={90}
-					/>
-					<SkillProgress
-						tooltipText={'Node.js'}
-						icon={IoLogoNodejs}
-						iconLabel={'Node.js logo'}
-						progressOnSkill={40}
-					/>
-					<SkillProgress
-						tooltipText={'React'}
-						icon={IoLogoReact}
-						iconLabel={'React logo'}
-						progressOnSkill={70}
-					/>
+					<HStack justify={'center'} spacing={0.5} wrap={'wrap'} mt={2}>
+						{listOfSkills.map(getSkillInfo)}
+					</HStack>
 				</Flex>
 			</Container>
 		</Flex>
